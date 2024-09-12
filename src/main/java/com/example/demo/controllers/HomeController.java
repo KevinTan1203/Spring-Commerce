7c40a1c4c84cab2a524ab4a803d98dc0ad2e1170
Kevin Tan YJ <58412870+KevinTan1203@users.noreply.github.com>
2024-09-12 03:26:31 +0000

    Building our first 2 routes

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String HelloWorld() {
+        return "<h1>Hello World!</h1>";
+    }
+
+    @GetMapping("/about-us")
+    @ResponseBody
+    public String AboutUs() {
+        return "<h1>About Us</h1>";
+    }
+}
